32690c84e8481be343e43fcffb0f6621fcf2ee2e
hh18076 <79485352+hh18076@users.noreply.github.com>
2021-11-26 14:21:42 +1300

    Add files via upload

---
+++ virusgamev1.py
@@ -0,0 +1,9 @@
+#User input
+country = str(input("Enter a country name"))
+
+print("The country is", country)
+
+#User output
+number = int(input("Enter the amount of people infected for the country"))
+             
+print("The amount of infected people in this country is", number)            
